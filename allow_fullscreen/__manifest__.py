04f937f012d7d98a4b65130a08b059da40b93afa
Andre Agung Purnomo <andrepurnomo04@gmail.com>
2020-04-16 10:40:40 +0700

    [ADD] Allow Fullscreen

---
+++ allow_fullscreen/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Allow Fullscreen',
+    'version': '1.0',
+    'summary': 'Allow fullscreen after add 2 home screen',
+    'category': 'mobile',
+    'author': 'Andre Agung Purnomo',
+    'maintainer': 'Andre Agung Purnomo',
+    'website': 'https://linkedin.com/in/andreagungpurnomo',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Andre Agung Purnomo',
+    ],
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/extend.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
